01964ef1bf85a9aee3fec7bd329a0e8a0028f3d0
Farhaan900 <mfarhaan.shaikh95@gmail.com>
2020-07-15 01:31:35 +0200

    fixed bash

--- extract.bash
+++ extract.bash
@@ -2,7 +2,7 @@ for i in `seq 1 64`;
 do
   echo worker $i
   # on cloud:
-  xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python run_evaluation_dopamine.py --base_dir="test_results" --gin_files="test_configs/rainbow.gin &
+  xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python run_evaluation_dopamine.py --base_dir="test_results" --gin_files="test_configs/rainbow.gin" &
   # on macbook for debugging:
   #python extract.py &
   sleep 1.0
